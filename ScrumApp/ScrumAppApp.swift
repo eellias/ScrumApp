8ff10ce29ed594d863380583e2a81ad57ab4e10a
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-07-10 15:38:49 +0300

    Initial Commit

---
+++ ScrumApp/ScrumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumAppApp.swift
+//  ScrumApp
+//
+//  Created by Ilya Tovstokory on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
