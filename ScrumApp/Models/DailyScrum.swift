091e0e0ee992667fbff89563c84c9a2af2de5152
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-07-12 15:07:19 +0300

    Basic UI for main view added. Card view added. Themes for cards added. Custom LabelStyle created.

---
+++ ScrumApp/Models/DailyScrum.swift
@@ -0,0 +1,32 @@
+//
+//  DailyScrum.swift
+//  ScrumApp
+//
+//  Created by Ilya Tovstokory on 12.07.2023.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let smapleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jehn", "Sarah"],
+                   lengthInMinutes: 5, theme: .poppy)
+    ]
+}
