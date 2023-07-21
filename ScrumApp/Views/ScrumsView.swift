c52bc954a16b2cf57160d0fca425936dceb75387
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-07-21 23:14:46 +0300

    Data persistence with FileManager added Async functions for loading and saving data added

--- ScrumApp/Views/ScrumsView.swift
+++ ScrumApp/Views/ScrumsView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentedNewScrum = false
+    let saveAction: () -> Void
     
     var body: some View {
         NavigationStack {
@@ -33,11 +35,15 @@ struct ScrumsView: View {
         .sheet(isPresented: $isPresentedNewScrum) {
             NewScrumSheet(isPresentingNewScrumView: $isPresentedNewScrum, scrums: $scrums)
         }
+        
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
     }
 }
